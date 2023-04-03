39d1660be508735b5a86b5880d61e9010e073c63
priyank rai <0priyankrai@gmail.com>
2023-04-03 11:04:39 +0530

    adding header

--- src/components/Views/Footer.tsx
+++ src/components/Views/Footer.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="flex justify-center items-center flex-col mb-24 h-screen space-y-5  ">
+      <p className="text-2xl italic underline">ThankYou For Visiting </p>
+      <p>
+        {" "}
+        Made with <span>&hearts;</span> in India{" "}
+      </p>
+      <p>Feel free to copy | No &copy; copyright issue</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
