8bcb037bad7085fe991b11ef237adb1b665dbde3
priyank rai <0priyankrai@gmail.com>
2023-04-05 14:45:10 +0530

    fixing some seo specific stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta
+          name="description"
+          content="Hi, I'm Priyank Rai, a 23-year-old full-stack web developer and DevOps enthusiast based in India. I hold a degree in Computer Science Engineering and I'm actively seeking software development opportunities. Explore my portfolio to see my skills and projects"
+        />
         <Script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
