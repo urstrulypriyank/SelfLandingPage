49aec39859b3e9a0fed06ff69d66e10c04c4f65a
priyank rai <0priyankrai@gmail.com>
2023-04-10 12:57:56 +0530

    added back home button in pdfs

--- src/components/pdfComponent/PdfComponent.jsx
+++ src/components/pdfComponent/PdfComponent.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const PdfComponent = ({ title, docUrl }) => {
@@ -6,8 +7,11 @@ const PdfComponent = ({ title, docUrl }) => {
     <>
       <div className="flex items-center justify-center h-full w-screen bg-gray-100">
         <div className="w-screen rounded-lg shadow-md overflow-hidden">
-          <div className="bg-gray-800 py-4 px-6 text-white">
+          <div className="bg-gray-800 py-4 px-6 text-white flex w-screen items-center justify-center flex-col">
             <h1 className="text-xl font-semibold text-center">{title}</h1>
+            <button className="bg-gray-600 m-2 p-2 rounded-md border border-green ">
+              <Link href="/">Home</Link>
+            </button>
           </div>
           <div className="bg-white py-6 px-6">
             <iframe
