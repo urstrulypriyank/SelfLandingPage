078c19b591998e6313d241cd75ee8d9fa88754bb
PRIYANK RAI <63704705+urstrulypriyank@users.noreply.github.com>
2024-04-30 15:30:56 +0530

    Update next.config.js
    bugfix: hsts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+    {
+    source: '/(.*)',
+    headers: [
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
